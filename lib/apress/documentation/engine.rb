fd80fa6bc2316d6b88f2e24e011269d54b70cab5
Denis Korobicyn <deniskorobitcin@gmail.com>
2017-05-04 16:57:04 +0500

    chore: add load_docs_for method

--- lib/apress/documentation/engine.rb
+++ lib/apress/documentation/engine.rb
@@ -4,12 +4,6 @@ module Apress
       config.autoload_paths += [
         config.root.join('app', 'controllers', 'concerns')
       ]
-      config.paths.add 'app/docs', eager_load: false
-
-      ActiveSupport.on_load(:documentation) do
-        glob = Apress::Documentation::Engine.config.root.join('app/docs/**/*.rb')
-        Dir[glob].each { |file| require file }
-      end
     end
   end
 end
